a3f429a12884056f50778d50bac4fb7f5283aa72
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-03 18:22:05 -0500

    class FIFOCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+class FIFOCache that inherits from BaseCaching and is a caching system
+
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    class FIFOCache inherits from class BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initialize class
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard_item = self.keys.pop(0)
+                del self.cache_data[discard_item]
+                print('DISCARD: {}'.format(discard_item))
+
+    def get(self, key):
+        """
+        Get an item by the key
+        """
+        if key and key in self.cache_data:
+            return (self.cache_data.get(key))
+        return None
