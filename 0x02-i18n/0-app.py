b277bf59dc928dd6c6cd97405fe0cad97f84fd1e
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-09 11:39:56 -0500

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,7 +13,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def home():
     """
     return 0-index.html
@@ -22,4 +22,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='5000')
