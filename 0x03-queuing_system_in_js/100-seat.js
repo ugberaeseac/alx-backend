d84edc1453acc986912ba02ca82c2ef22acd3972
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-03-06 19:54:26 -0500

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -0,0 +1,64 @@
+import redis from 'redis';
+import { promisify } from "util";
+import kue from 'kue'
+const express = require("express");
+
+const client = redis.createClient();
+const app = express();
+const queue = kue.createQueue();
+
+function reserveSeat(number) {
+  client.set("available_seats", number);
+}
+
+const get = promisify(client.get).bind(client);
+
+async function getCurrentAvailableSeats() {
+  return await get("available_seats");
+}
+
+reserveSeat(50);
+let reservationEnabled = true;
+
+app.get("/available_seats", (req, res) => {
+  getCurrentAvailableSeats().then((resp) =>
+    res.json({ numberOfAvailableSeats: resp })
+  );
+});
+
+app.get("/reserve_seat", (req, res) => {
+  if (!reservationEnabled) {
+    res.json({ status: "Reservation are blocked" });
+  } else {
+    let job = queue.create("reserve_seat");
+    job
+      .on("complete", () =>
+        console.log(`Seat reservation job ${job.id} completed`)
+      )
+      .on("failed", (err) =>
+        console.log(`Seat reservation job ${job.id} failed: ${error}`)
+      )
+      .save((error) => {
+        if (error) {
+          res.json({ status: "Reservation failed" });
+	} else {
+	  res.json({ status: "Reservation in process" });
+	  }
+      });
+  }
+});
+
+app.get("/process", (req, res) => {
+  queue.process("reserve_seat", (job, done) => {
+    getCurrentAvailableSeats().then((resp) => {
+      if (resp == 1) reservationEnabled = false;
+      if (resp >= 1) {
+        reserveSeat(resp - 1);
+        done();
+      } else done(new Error("Not enough seats available"));
+    });
+  });
+  res.json({ status: "Queue processing" });
+});
+
+app.listen("1245");
